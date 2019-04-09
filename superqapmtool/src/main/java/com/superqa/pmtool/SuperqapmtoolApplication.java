34ed457bfeaa22e331c1ca0fcc919ea8920df603
samuel habtemariam <samuel.habt@gmail.com>
2019-04-09 00:09:02 -0700

    initial commit

---
+++ superqapmtool/src/main/java/com/superqa/pmtool/SuperqapmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.superqa.pmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperqapmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperqapmtoolApplication.class, args);
+	}
+
+}
